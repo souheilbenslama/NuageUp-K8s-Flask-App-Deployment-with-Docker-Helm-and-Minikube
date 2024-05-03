43ad31f8e1e1a4ce0374565d8e0efd2a3d3ef384
souheilbenslama <benslamasouheil@gmail.com>
2024-05-03 22:12:21 +0100

    create docker , deployment , service

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10-slim-buster
+
+WORKDIR /app
+COPY ./requirements.txt /app
+RUN pip install -r requirements.txt
+COPY . .
+EXPOSE 5000
+ENV FLASK_APP=app.py
+CMD [ "flask", "run", "--host", "0.0.0.0" ]
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"Hello, {escape(name)}!"
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/healthz")
+def health_check():
+    return 'OK', 200
+
+@app.route("/ready")
+def ready_check():
+    return 'OK', 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
